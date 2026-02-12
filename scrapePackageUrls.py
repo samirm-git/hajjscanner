326af4f588925e7410429eba212e5f304d1c7969
Samir Mahmood <samirmalik7@hotmail.co.uk>
2026-02-12 10:17:38 +0000

    using 10 links as a threshold to decide if a page is a catelogue page

--- scrapePackageUrls.py
+++ scrapePackageUrls.py
@@ -3,58 +3,80 @@ from helpers import makeRequest, getSoup, removeFooterHeaderNav
 from pprint import pprint
 
 
-def scrapeFromSitemap(soup):
-  hajjPackages = []
-  umrahPackages = []
+def scrapeLinksSitemap(soup):
+  hajjPackageUrls = []
+  umrahPackageUrls = []
   for urlTag in soup.find_all("url"):
     url = urlTag.find("loc").get_text(strip=True)
-    #TODO: MAKE THIS MORE ROBUST AS AN RE SEARCH OVER 'hajjpackage' , 'hajj_package', ...
+    #TODO: IF last-modified IS VERY OLD DON'T USE IT
     if re.search(r"hajj[-_]?package", url, re.IGNORECASE):
       lastModified = urlTag.find("lastmod").get_text(strip=True)
-      hajjPackages.append({'url':url, 'last-modified': lastModified})
+      hajjPackageUrls.append({'url':url, 'last-modified': lastModified})
     elif re.search(r"umrah?[-_]?package", url, re.IGNORECASE):
       lastModified = urlTag.find("lastmod").get_text(strip=True)
-      umrahPackages.append({'url': url, 'last-modified': lastModified})
+      umrahPackageUrls.append({'url': url, 'last-modified': lastModified})
   
-  return (hajjPackages, umrahPackages)
+  return (hajjPackageUrls, umrahPackageUrls)
     
-def scrapeFromHomePage(soup):
+def scrapeLinksHomePage(soup):
   print("hi")
 
   return None, None
 
 def filterPackages(packageList):
+  packageUrls, caltalougeUrls = [], []
   for package in packageList:
     resp = makeRequest(package['url'])
     soup = getSoup(resp.text, 'lxml')
     soup = removeFooterHeaderNav(soup)
     numberOfLinks = len(soup.find_all("a", href=True))
     if numberOfLinks > 10:
+      caltalougeUrls.append(package['url'])
       print(f"CATALOGUE PAGE: {package['url']} :  {numberOfLinks}")
     else:
+      packageUrls.append(package['url'])
       print(f"PACKAGE PAGE: {package['url']} :  {numberOfLinks}")
+  
+  return packageUrls, caltalougeUrls
+
+
+def scrapePackages(packageUrls):
+  for package in packageUrls:
+    continue
+  ##TODO: CALL THE SCRAPER.PY ON ALL THE PACKAGES 
+  return 0
+
 
 with open('providers.txt','r') as f:
   providerList = f.read().splitlines()
 
-alHaqSitemap = providerList[2] + "sitemap.xml"
-print(alHaqSitemap)
-resp = makeRequest(alHaqSitemap)
+sitemapurl = providerList[2] + "sitemap.xml"
+
+print(sitemapurl)
+resp = makeRequest(sitemapurl)
 
 match resp.status_code:
   case 200 | 304:
     soup = getSoup(resp.text, parser="xml")
-    hajjPackages, umrahPackages = scrapeFromSitemap(soup)
+    hajjPackageUrls, umrahPackageUrls = scrapeLinksSitemap(soup)
   case 301:
     soup = getSoup(resp.text, parser="lxml")
-    hajjPackagesj, umrahPackages = scrapeFromHomePage(soup)
+    hajjPackageUrls, umrahPackageUrls = scrapeLinksHomePage(soup)
 
 print("")
 print("")
-hajjPackages = filterPackages(hajjPackages)
-# umrahPackages = filterPackages(umrahPackages)
+hajjPackageUrls, hajjCatalogueUrls = filterPackages(hajjPackageUrls)
+umrahPackageUrls, umrahCatalogueUrls = filterPackages(umrahPackageUrls)
 
-print(hajjPackages)
 
-## if there is multiple links on the page then we know this is not for a specific package.
-## is there a more sophisticated way to differentiate between a catalogue page and a specific package details page
+if hajjPackageUrls:
+  out = scrapePackages(hajjPackageUrls)
+  if len(out) < 6:
+    scrapeLinksHomePage(hajjCatalogueUrls)
+
+if umrahPackageUrls:
+  out = scrapePackages(umrahPackageUrls)
+  if len(out) < 6:
+    scrapeLinksHomePage(umrahCatalogueUrls)
+# umrahPackageUrls = filterPackages(umrahPackageUrls)
+
